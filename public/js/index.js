f9dca3d4387b95a715e14f2fc36f22dc56a5f134
Pancake User <coffeepancake2015@gmail.com>
2018-11-18 20:56:02 +0800

    scroll to bottom when user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom(){
+    //  Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    //  Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connecteed to server');
 
@@ -27,7 +45,7 @@ socket.on('newMessage', function(message){
     });
 
     $('#messages').append(html);
-    
+    scrollToBottom();
     // var formattedTime = moment(message.createdAt).format('h:mm a');
     
     // var li = $('<li></li>');
@@ -46,6 +64,8 @@ socket.on('newLocationMessage', function(message){
     });
 
     $('#messages').append(html);
+    scrollToBottom();
+
     // var formattedTime = moment(message.createdAt).format('h:mm a');
 
     // var li = $('<li></li>');
